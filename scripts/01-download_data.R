ad515d061161bd6302ac5caa38aaeba20a16cd33
thejasonminh <123194102+thejasonminh@users.noreply.github.com>
2023-04-05 01:33:53 -0400

    added xl files, wrote initial submission

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,36 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Data: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads data from the NHL API from seasons 2011-2012 to 2022-2023
+# Author: Jason Ngo
+# Data: Apr 4 2023
+# Contact: jason_ngo@live.com
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: A working internet connection
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(here)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
+## Manually downloaded excel files by exporting from nhl.com/stats/teams and sorting by seasons. ##
 
+# I acknowledge that the nhl api exists and is a usable R package, however.. #
+# ..isolating specific statistics by teams during a 10 year period using the API was too difficult #
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+
+teams2223 <- read_excel(here::here("inputs/data/Summaryteam_2223.xlsx"))
+teams2122 <- read_excel(here::here("inputs/data/Summaryteam_2122.xlsx"))
+teams2021 <- read_excel(here::here("inputs/data/Summaryteam_2021.xlsx"))
+teams1920 <- read_excel(here::here("inputs/data/Summaryteam_1920.xlsx"))
+teams1819 <- read_excel(here::here("inputs/data/Summaryteam_1819.xlsx"))
+teams1718 <- read_excel(here::here("inputs/data/Summaryteam_1718.xlsx"))
+teams1617 <- read_excel(here::here("inputs/data/Summaryteam_1617.xlsx"))
+teams1516 <- read_excel(here::here("inputs/data/Summaryteam_1516.xlsx"))
+teams1415 <- read_excel(here::here("inputs/data/Summaryteam_1415.xlsx"))
+teams1314 <- read_excel(here::here("inputs/data/Summaryteam_1314.xlsx"))
+teams1213 <- read_excel(here::here("inputs/data/Summaryteam_1213.xlsx"))
+teams1112 <- read_excel(here::here("inputs/data/Summaryteam_1112.xlsx"))
 
          
